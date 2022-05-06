aa2db322be0a35787602c03f225c4a3e8e8efc2f
solilov <alexandr.solilov@yandex.ru>
2022-05-06 17:31:18 +0300

    Создан файл test_items.py, в нем описан тест с поиском кнопки.

---
+++ test_items.py
@@ -0,0 +1,19 @@
+import time
+
+from selenium.webdriver.common.by import By
+
+
+link = 'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/'
+
+
+def test_сhecking_for_a_button(browser):
+    browser.get(link)
+    button = browser.find_elements(
+        By.XPATH,
+        '//button[@class="btn btn-lg btn-primary btn-add-to-basket"]'
+    )
+    time.sleep(10)
+    if not button:
+        assert button, (
+            'The button is missing, check if the element search selector is correct'
+        )
