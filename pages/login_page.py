91bd5f33755b4e2e9bf9e1de3b7caca8cd7f3eb9
solilov <alexandr.solilov@yandex.ru>
2022-05-11 15:00:57 +0300

    Добавлены локаторы, изменены методы в login_page.py

--- pages/login_page.py
+++ pages/login_page.py
@@ -1,4 +1,5 @@
 from .base_page import BasePage
+from .locators import LoginPageLocators
 
 
 class LoginPage(BasePage):
@@ -8,13 +9,15 @@ class LoginPage(BasePage):
         self.should_be_register_form()
 
     def should_be_login_url(self):
-        # реализуйте проверку на корректный url адрес
-        assert True
+        assert self.url in self.browser.current_url, 'Not in current url'
 
     def should_be_login_form(self):
-        # реализуйте проверку, что есть форма логина
-        assert True
+        assert self.is_element_present(
+            *LoginPageLocators.LOGIN_FORM
+        ), 'Login form is not found'
 
     def should_be_register_form(self):
-        # реализуйте проверку, что есть форма регистрации на странице
-        assert True
+        assert self.is_element_present(
+            *LoginPageLocators.REGISTER_FORM
+        ), 'Register form is not found'
+
