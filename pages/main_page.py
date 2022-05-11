4b37da4bb62dfb6c60d6072b4a98c67753fe5687
solilov <alexandr.solilov@yandex.ru>
2022-05-11 17:24:02 +0300

    Добавлен метод в класс BasePage для вычисления значения в алерте

--- pages/main_page.py
+++ pages/main_page.py
@@ -1,15 +1,13 @@
-from selenium.webdriver.common.by import By
-
 from .base_page import BasePage
 from .locators import MainPageLocators
-from .login_page import LoginPage
 
 
 class MainPage(BasePage):
     def go_to_login_page(self):
-        link = self.browser.find_element(*MainPageLocators.LOGIN_LINK)
+        link = self.browser.find_element(
+            *MainPageLocators.LOGIN_LINK
+        )
         link.click()
-        return LoginPage(browser=self.browser, url=self.browser.current_url)
 
     def should_be_login_link(self):
         assert self.is_element_present(
