ee2642e70660944adfe3b41edb091f617daa336f
solilov <alexandr.solilov@yandex.ru>
2022-05-13 12:51:15 +0300

    Добавлены локат оры и вненсены изменения в файл product_page.py

--- pages/locators.py
+++ pages/locators.py
@@ -1,13 +1,25 @@
 from selenium.webdriver.common.by import By
 
 
+class BasePageLocators():
+    LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
+    LOGIN_LINK_INVALID = (By.CSS_SELECTOR, "#login_link_inc")
+    BASKET_BUTTON = (By.XPATH,'//*[@id="default"]/header/div[1]/div/div[2]/span/a')
+    USER_ICON = (By.CSS_SELECTOR, ".icon-user")
+
+
 class MainPageLocators():
     LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
 
 
 class LoginPageLocators():
-    LOGIN_FORM = (By.ID, "login_form")
-    REGISTER_FORM = (By.ID, "register_form")
+    LOGIN_FORM = (By.ID, 'login_form')
+    REGISTER_FORM = (By.ID, 'register_form')
+    EMAIL = (By.ID, "id_registration-email")
+    PASSWORD = (By.ID, "id_registration-password1")
+    CONF_PASSWORD = (By.ID, "id_registration-password2")
+    BTN_REG = (By.NAME, "registration_submit")
+    LOG_OUT = (By.ID, "logout_link")
 
 
 class ProductPageLocators():
@@ -16,3 +28,8 @@ class ProductPageLocators():
     BOOK_TITLE = (By.CSS_SELECTOR, ".product_main h1")
     BOOK_PRICE = (By.CSS_SELECTOR, ".product_main p.price_color")
     BASKET_TOTAL = (By.CSS_SELECTOR, ".alert-info strong")
+
+
+class BasketPageLocators():
+    TEXT_BASKET_EMPTY = (By.CSS_SELECTOR, "#content_inner a")
+    ITEMS_LIST = (By.CLASS_NAME, 'basket-items')
--- pages/product_page.py
+++ pages/product_page.py
@@ -19,3 +19,38 @@ class ProductPage(BasePage):
         basket_total = self.browser.find_element(*Locators.BASKET_TOTAL).text
         msg = f'Wrong basket total: expected "{price}", got "{basket_total}"'
         assert basket_total == price, msg
+
+    def press_button_add_to_basket(self):
+        # Нажимаем кнопку Add to basket
+        button_add_to_basket = self.browser.find_element(*Locators.BUTTON_ADD_TO_BASKET)
+        button_add_to_basket.click()
+
+    def should_be_message_about_adding(self):
+        # Сначала проверяем, что элементы присутствуют на странице
+        assert self.is_element_present(*Locators.PRODUCT_NAME), "Product name is not presented"
+        assert self.is_element_present(*Locators.MESSAGE_ABOUT_ADDING), "Message about adding is not presented"
+        # Затем получаем текст элементов для проверки
+        product_name = self.browser.find_element(*Locators.PRODUCT_NAME).text
+        message = self.browser.find_element(*Locators.MESSAGE_ABOUT_ADDING).text
+        # Проверяем, что название товара присутствует в сообщении о добавлении
+        assert product_name == message, "Product name is wrong in the message"
+        #assert product_name in message, "No product name in the message"
+
+    def should_be_message_basket_total(self):
+        # Сначала проверяем, что элементы присутствуют на странице
+        assert self.is_element_present(*Locators.MESSAGE_BASKET_TOTAL), "Message basket total is not presented"
+        assert self.is_element_present(*Locators.PRODUCT_PRICE), "Product price is not presented"
+        # Затем получаем текст элементов для проверки
+        message_basket_total = self.browser.find_element(*Locators.MESSAGE_BASKET_TOTAL).text
+        product_price = self.browser.find_element(*Locators.PRODUCT_PRICE).text
+        # Проверяем, что цена товара присутствует в сообщении со стоимостью корзины
+        assert product_price == message_basket_total, "Price is wrong in the message"
+        #assert product_price in message_basket_total, "No product price in the message"
+
+    def should_not_be_success_message(self):
+        assert self.is_not_element_present(*Locators.MESSAGE_ABOUT_ADDING), \
+            "Success message is presented, but should not be"
+
+    def element_disappeared(self):
+        assert self.is_disappeared(*Locators.MESSAGE_ABOUT_ADDING), \
+            "Element did not disappear"
